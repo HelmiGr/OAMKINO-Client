dbb4c638932e09570267bac67e24fce2f652a9a9
Ha Nguyen <c3ngth01@student.oamk.fi>
2024-12-13 12:16:19 +0200

    add ulr api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Shift this variable to an environment file in production
-export const BASE_API_URL = "http://localhost:3001/";
+export const BASE_API_URL = "https://web-ad-ser-tryied.onrender.com/";
 
 export const apiClient = axios.create({
   baseURL: BASE_API_URL,
