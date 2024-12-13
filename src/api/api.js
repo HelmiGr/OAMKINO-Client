990c365869823a072144e8095b63bbc5bfb262e3
Ha Nguyen <c3ngth01@student.oamk.fi>
2024-12-13 13:26:59 +0200

    BASE_API_URL deploy in render

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Shift this variable to an environment file in production
-export const BASE_API_URL = "https://web-ad-ser-tryied.onrender.com/";
+export const BASE_API_URL = "https://oamkino-server.onrender.com/";
 
 export const apiClient = axios.create({
   baseURL: BASE_API_URL,
